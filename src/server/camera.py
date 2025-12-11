58caa0756bd40e9678b4829232bc20fa50a71efa
DiamondDev <fynndiamond@gmail.com>
2025-12-11 16:57:04 +0000

    remove picamera
    i think a mutilated usb webcam would work better

--- src/server/camera.py
+++ src/server/camera.py
@@ -1,7 +1,5 @@
 import cv2
-import time
 import numpy as np
-from picamera2 import PiCamera2 # i am coding this blind lol
 
 from src.common import consts
 
@@ -13,10 +11,6 @@ class CameraFeed():
         self.cam_id = cam_id
         
         self.camera = cv2.VideoCapture(self.cam_id) # get the opencv camera feed
-        
-        self.picam = PiCamera2()
-        self.picam.start()
-        time.sleep(0.1) # camera warmup
 
 
     def capture(self, cam_quality: int = consts.CAMERA_JPEG_COMPRESSION_VALUE) -> bytes:
@@ -24,12 +18,10 @@ class CameraFeed():
         gets and encodes a frame from the camera. returns the bytes of the encoded JPEG
         """
         # fetch frame
-        #success, frame = self.camera.read()
-        #if not success:
-        #    print("failed frame read")
-        #    return b''
-
-        success, frame = True, self.picam.capture_array()
+        success, frame = self.camera.read()
+        if not success:
+            print("failed frame read")
+            return b''
 
         # encode
         success, frame = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), cam_quality])
